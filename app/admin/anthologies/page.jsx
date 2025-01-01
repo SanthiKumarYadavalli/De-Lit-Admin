14cf55f27eb4b6eca6d5f9170e02b9cadadce789
saijayanth59 <sithlord5995@gmail.com>
2025-01-01 20:41:57 +0530

    feat: add article and maganzine forms

--- app/admin/anthologies/page.jsx
+++ app/admin/anthologies/page.jsx
@@ -1,6 +1,6 @@
 import AnthologyForm from "@/components/form/AnthologyForm";
 import PageWrapper from "@/components/PageWrapper";
-
+import AnthologyForm from "@/components/form/AnthologyForm";
 export default function page() {
   return <PageWrapper title={"Anthologies"} itemName="Anthology" FormComponent={AnthologyForm} />;
 }
