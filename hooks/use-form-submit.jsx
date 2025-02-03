e1af9df10064bc81a877f738c81758f44cc6b85e
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-02-03 17:52:07 +0530

    feat: refresh page after form submission

--- hooks/use-form-submit.jsx
+++ hooks/use-form-submit.jsx
@@ -1,5 +1,7 @@
+import { useRouter } from "next/navigation";
 
 export default function useFormSubmit(setIsLoading, setIsOpen) {
+  const router = useRouter();
   return async function submitForm(apiCall) {
     try {
       setIsLoading(true);
@@ -8,6 +10,7 @@ export default function useFormSubmit(setIsLoading, setIsOpen) {
       if (setIsOpen) {
         setIsOpen(false);
       }
+      router.refresh();
     } catch (error) {
       console.error(error);
     }
